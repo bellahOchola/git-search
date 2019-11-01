9ec0370e4cb223b12591de3fd8c10214c45763bc
bellahOchola <bellahkenya@gmail.com>
2019-11-01 12:39:08 +0300

    create new component:user-dtails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GittyComponent } from './gitty/gitty.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GittyComponent
+    GittyComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
