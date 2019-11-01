73fc5fa60751b132474407ed710b5bb06b7e2429
bellahOchola <bellahkenya@gmail.com>
2019-11-01 10:55:38 +0300

    add parameters to the constructor and import the class to parent ts file

--- src/app/gitty/gitty.component.ts
+++ src/app/gitty/gitty.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Repository } from '../class/repository';
 @Component({
   selector: 'app-gitty',
   templateUrl: './gitty.component.html',
   styleUrls: ['./gitty.component.css']
 })
 export class GittyComponent implements OnInit {
+  repository:Repository[]=[
 
+  ]
   constructor() { }
 
   ngOnInit() {
